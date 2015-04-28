38e76a37d37f75b5e74313262741731eb7efb1a8
simonvea <Simon@Simons-MacBook-Air-2.local>
2015-04-28 21:22:33 +0200

    size is now workin

--- skript.js
+++ skript.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-    var x = 16*16;
-    for(var i=0;i<x;i++){
+    var x = 32;
+    var y = (500/x)-8;
+    for(var i=0;i<x*x;i++){
         $("#innhold").append("<div></div>");
         
+        
     }
+    $("#innhold > div").css("width", y + "px");
+    $("#innhold > div").css("height", y  + "px" );
     $("#innhold > div").hover(function() {
        $(this).addClass("hover");
     });
