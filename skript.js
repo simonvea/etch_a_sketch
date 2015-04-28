17346b1c9b46c706bb9eca395a68c3377f7f18cd
simonvea <Simon@Simons-MacBook-Air-2.local>
2015-04-28 22:24:04 +0200

    Everything is working

--- skript.js
+++ skript.js
@@ -1,6 +1,39 @@
+var x = 16;
+
+function reset() {
+    //$("div").removeClass("hover");
+   
+    };
+
 $(document).ready(function() {
-    var x = 32;
+    
+    $("#button").click(function() {
+        var x = prompt("Skriv inn størrelse på ramme i antall bokser  (f.eks 16 vil gi en box med 16x16) (minimum 16)")
+        $("#innhold").empty();
+        
+        if(x<16) {x = 16};
+        
+        var y = (500/x)-8;
+    
+        for(var i=0;i<x*x;i++){
+        $("#innhold").append("<div></div>");
+        
+        }
+        
+        $("#innhold > div").css("width", y + "px");
+        $("#innhold > div").css("height", y  + "px" );
+        $("#innhold > div").hover(function() {
+            $(this).addClass("hover");
+        });
+        
+    });  
+    
+    
     var y = (500/x)-8;
+    
+    //if(x<16) {x = 16};
+    
+    
     for(var i=0;i<x*x;i++){
         $("#innhold").append("<div></div>");
         
@@ -11,10 +44,6 @@ $(document).ready(function() {
     $("#innhold > div").hover(function() {
        $(this).addClass("hover");
     });
-    
-    
+       
 });
 
-function reset() {
-        $("div").removeClass("hover");
-    };
